816602e05bdfbef8d5306ca3592fa0d4df538c5e
saisai <you@example.com>
2020-08-19 06:31:21 +0700

    init

--- thjobstopgun/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import JobsTopGun
-
-
-class JobsTopGunSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = JobsTopGun
-        fields = ('pk','title',  'link')
--- thjobstopgun/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from thjobstopgun import views
-
-urlpatterns = [
-        url(r'^$', views.index, name='thjobs-topgun'),
-        url(r'^testajax/$', views.testajax, name='testajax'),
-        ]
